47f84a6a67b5bff4034b97eb181949bb7d3af04a
Hygor Martins <53188282+HygorM10@users.noreply.github.com>
2020-07-18 16:34:44 -0300

    Add Fieldset de Itens de Coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,5 @@
+const collectedItems = document.querySelector("input[name=items]");
+
 function populaUFs(){
     const ufSelect = document.querySelector("select[name=uf]");
 
@@ -23,11 +25,15 @@ function getCidades(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+
+    citySelect.disabled = false
+
     fetch(url)
         .then( res => res.json() )
         .then( cities => {
             for( const city of cities){
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false;
@@ -37,3 +43,38 @@ function getCidades(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCidades);
+
+// Ítens de Coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for(const item of itemsToCollect){
+    item.addEventListener("click", handleItemClick);
+}
+
+let selectedItems = [];
+
+function handleItemClick(event) {
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex(item => {
+        return item == itemId;
+    });
+
+    if(alreadySelected >= 0){
+        const filterItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+        });
+
+        selectedItems = filterItems;
+    }else{
+        selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems;
+
+}
