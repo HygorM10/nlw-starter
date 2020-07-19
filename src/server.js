cec8d89be1f8f7d27841ec6d4c8036a653edc21a
Hygor Martins <53188282+HygorM10@users.noreply.github.com>
2020-07-19 16:04:15 -0300

    Implementando nunjucks e eliminando as repetições nas paginas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const server = express();
 
+//Utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
+//Configurar pasta public
+server.use(express.static("public"));
+
 //Ligar o servidor
 server.listen(3000);
+
+//Caminhos da aplicação 
+
+//Pagina Inicial
+//Require: Requisição ao Servidor
+//Response: Resposta do Servidor
+server.get("/", (require, response) => {
+    return response.render("index.html");
+});
+
+server.get("/create-point", (require, response) => {
+    return response.render("create-point.html");
+});
+
+server.get("/search", (require, response) => {
+    return response.render("search-results.html");
+});
